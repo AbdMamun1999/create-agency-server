d861c4c272cbbe6440711382be03c956ef539128
Abdullah Al Mamun <mamun12121999@gmail.com>
2022-08-07 11:15:32 +0600

    create an api for single product

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jdwljml.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -19,10 +19,17 @@ async function run() {
         const productCollection = client.db('ecommerce-product').collection('all-product')
 
 
-        app.get('/products',async(req,res)=>{
+        app.get('/products', async (req, res) => {
             const query = {}
             const cursor = productCollection.find(query)
             const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: (ObjectId(id)) }
+            const result = await productCollection.findOne(query)
             res.send(result)
         })
     }
